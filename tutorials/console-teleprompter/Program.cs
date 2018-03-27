8a6efad228738b99ee451432e459ee85d6b55225
Michael Christenson <github@pdxchristenson.com>
2018-03-27 15:13:26 -0700

    First step of the TeleprompterConsole exercise.

---
+++ tutorials/console-teleprompter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TeleprompterConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = ReadFrom("sampleQuotes.txt");
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        static IEnumerable<string> ReadFrom(string file)
+        {
+            string line;
+            using (var reader = File.OpenText(file))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    yield return line;
+                }
+            }
+        }
+    }
+}
