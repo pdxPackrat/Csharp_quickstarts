1152de2f3caccaa9b48bea0e42b21e509209e57a
Michael Christenson <mdc88@pdxchristenson.com>
2018-04-04 15:22:05 -0700

    Continuation of the PluralSight course
    In the "Assemblies" part of the course, we added more changes to the Grades project and we were introduced to the concept of unit tests.

--- vs2015_stuff/Grades/Grades/Program.cs
+++ vs2015_stuff/Grades/Grades/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+// using System.Speech.Synthesis;
 
 namespace Grades
 {
@@ -10,10 +11,18 @@ namespace Grades
     {
         static void Main(string[] args)
         {
+//          SpeechSynthesizer synth = new SpeechSynthesizer();
+
+//          Console.WriteLine("What is your name, user?");
+//          String myName = Console.ReadLine();
+
+//          synth.Speak("Hello " + myName +" , this is the grade book program");
+
             GradeBook book = new GradeBook();
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
+
 
             GradeStatistics stats = book.ComputeStatistics();
             Console.WriteLine("Avg: " + stats.AverageGrade);
